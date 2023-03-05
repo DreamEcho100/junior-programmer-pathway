c792cf7717f5466736aea0160c8828515217ee2d
DreamEcho100 <maze6572198@gmail.com>
2023-03-06 00:57:43 +0200

    Prototype 1 -> Bonus Features 1 - Share your Work

---
+++ Prototype 1/Assets/Scripts/OncomingVehicleController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OncomingVehicleController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -7,22 +7,60 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed = 15.0f, turnSpeed = 60.0f, turnSpeedTrimmed;
 
+    [SerializeField]
+    private int playerNumber;
+
+    private Dictionary<int, List<KeyCode>> keyMap = new()
+    {
+        {
+            1,
+            new List<KeyCode>() {
+                KeyCode.W,
+                KeyCode.D,
+                KeyCode.S,
+                KeyCode.A,
+            }
+        },
+        {
+            2,
+            new List<KeyCode>() {
+                KeyCode.UpArrow,
+                KeyCode.RightArrow,
+                KeyCode.DownArrow,
+                KeyCode.LeftArrow,
+            }
+        },
+    };
+    private List<KeyCode> playerKeys;
     private float horizontalInput, forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerKeys = keyMap[playerNumber];
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        
-        turnSpeedTrimmed = Time.deltaTime * turnSpeed * horizontalInput;
+        var upKey = playerKeys[0];
+        var downKey = playerKeys[2];
+        forwardInput = Input.GetKey(upKey) ? 1 : Input.GetKey(downKey) ? -1 : 0;
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeedTrimmed);
+        if (forwardInput != 0)
+        {
+            transform.Translate(
+                Vector3.forward * Time.deltaTime * speed * forwardInput
+            );
+        }
+
+        var rightKey = playerKeys[1];
+        var leftKey = playerKeys[3];
+        horizontalInput = Input.GetKey(rightKey) ? 1 : Input.GetKey(leftKey) ? -1 : 0;
+
+        if (horizontalInput != 0)
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        }
     }
 }
